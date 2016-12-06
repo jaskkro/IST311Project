f702f3bc8697e569c3b96a28d6620976955d8dd6
benthekatz <benthekatz@gmail.com>
2016-12-05 22:50:28 -0500

    12/5/16 - Updated MainUI and NewCredentialUI
    Transferred generate password functionality to NewCredentialUI - user
    will now be able to click on "generate password" and it will
    automatically be placed in the Password textfield

--- IST 311 Project/src/MainUI.java
+++ IST 311 Project/src/MainUI.java
@@ -18,11 +18,10 @@ public class MainUI extends JFrame {
   DefaultTableModel model = new DefaultTableModel(new Object[][] {
       { "some", "text", "fill", "in" }, { "any", "text", "for" ,"credentials" }, { "even", "more" },
       { "text", "strings" }, { "and", "other" }, { "text", "values" } },
-      new Object[] { "Credential Type", "Credential Username","Credential Password","Credential Custom" });
+      new Object[] { "Username", "Password","Type","Notes" });
   private static final int FRAME_WIDTH = 600;
     private static final int FRAME_HEIGHT = 400;
   private JButton CreateNewCredential;
-  private JButton password;
   private JButton Logout;
 
   public MainUI() {
@@ -31,46 +30,27 @@ public class MainUI extends JFrame {
     JTable table = new JTable(model);
     getContentPane().add(new JScrollPane(table), BorderLayout.CENTER);
     CreateNewCredential = new JButton("Create New Credential");
-    password = new JButton("Generate Rondom Password");
-    password.addActionListener(new ActionListener() {
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			JOptionPane.showMessageDialog(null,"Random password: "+genRandomPwd());
-		}
-	});
+    CreateNewCredential.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent ae) {
+             new NewCredentialUI().setVisible(true);
+          }
+    });
+    Logout = new JButton("Logout");
+    Logout.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent ae) {
+              
+          }
+        
+    });
+    
+    
     JPanel jp = new JPanel();
     jp.add(CreateNewCredential);
-    jp.add(password);
+    jp.add(Logout);
     this.add(jp, BorderLayout.SOUTH);
     
 
   }
-
-  public static String genRandomPwd(){
-	  
-	  final int  maxNum = 62;
-	  int i;  
-	  int count = 0; 
-	  char[] str = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-	    'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-	    'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-	    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-	    'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-	  
-	  StringBuffer pwd = new StringBuffer("");
-	  Random r = new Random();
-	  while(count < 15){
-
-	   
-	   i = Math.abs(r.nextInt(maxNum));  
-	   
-	   if (i >= 0 && i < str.length) {
-	    pwd.append(str[i]);
-	    count ++;
-	   }
-	  }
-	  
-	  return pwd.toString();
-	 }
 }
---
+++ IST 311 Project/src/NewCredentialUI.java
@@ -0,0 +1,153 @@
+
+import java.awt.Color;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import javax.swing.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Ethan
+ */
+public class NewCredentialUI extends JFrame implements ActionListener{
+    private static final int FRAME_WIDTH = 450;
+    private static final int FRAME_HEIGHT = 450;
+    private MainUI Main;
+    private JButton CreateCredential;
+    private JButton GenPassword;
+    private JButton Cancel;
+    
+    private JLabel CredentialTypeLabel;
+    private JTextField CredentialType;
+    private JLabel UsernameCredentialLabel;
+    private JTextField CredentialUsername;
+    
+    private JLabel CredentialPasswordLabel;
+    private JTextField CredentialPassword;
+    
+    private JLabel CredentialEmailLabel;
+    private JTextField CredentialEmail;
+
+    private JLabel CredentialUserNoteLabel;
+    private JTextField CredentialNote;
+    
+    /**
+     * Creates new form NewJFrame
+     */
+    public NewCredentialUI() {
+            this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+            //this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setVisible(true);
+            
+            //TextFields
+            CredentialUsername = new JTextField(20);
+            CredentialPassword = new JTextField(20);
+            CredentialType = new JTextField(20);
+            CredentialEmail = new JTextField(20);
+            CredentialNote = new JTextField(20);
+            
+            //JLabels
+            UsernameCredentialLabel = new JLabel("Username:");
+            CredentialPasswordLabel = new JLabel("Password:");
+            CredentialTypeLabel = new JLabel("Type:");
+            CredentialEmailLabel = new JLabel("Email:");
+            CredentialUserNoteLabel = new JLabel("Notes:");
+            
+            //Jbuttons
+            CreateCredential = new JButton("Submit");
+            Cancel = new JButton("Cancel");
+            GenPassword = new JButton("Generate Random Password");
+            CreateCredential.addActionListener(this);
+            GenPassword.addActionListener(this);
+            Cancel.addActionListener(this);
+            this.add(createPanel());
+            
+            
+            
+
+        
+    }
+    private JPanel createPanel() 
+	{
+		JPanel panel = new JPanel(new GridLayout(6,2));
+                panel.add(UsernameCredentialLabel);
+		panel.add(CredentialUsername);
+                panel.add(CredentialPasswordLabel);
+                panel.add(CredentialPassword);
+                panel.add(CredentialTypeLabel);
+                panel.add(CredentialType);
+                panel.add(CredentialEmailLabel);
+                panel.add(CredentialEmail);
+                panel.add(CredentialUserNoteLabel);
+		panel.add(CredentialNote);
+                panel.add(CreateCredential);
+                panel.add(GenPassword);
+		return panel;
+	}
+    
+         
+        public void actionPerformed(ActionEvent ac) {
+             if(ac.getSource()== CreateCredential)
+            {
+                 String credentialType = CredentialType.getText();
+                 String username = CredentialUsername.getText();
+                 String email = CredentialEmail.getText();
+                 String credentialPassword = CredentialPassword.getText();
+                 String note = CredentialNote.getText();
+                 Credential credential = new Credential(credentialType, username, email, new Password(credentialPassword), note);
+             }
+             //password button listener
+             if(ac.getSource()== GenPassword)
+             {
+		//JOptionPane.showMessageDialog(null,"Random password: "+ genRandomPwd());
+                 String temp_gen = genRandomPwd();
+                CredentialPassword.setText(temp_gen);
+             }
+             
+            if(ac.getSource()== Cancel){
+                System.out.println("Cancel"); 
+            }
+    
+            
+        }
+        
+    public static String genRandomPwd() {
+
+        final int maxNum = 62;
+        int i;
+        int count = 0;
+        char[] str = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+            'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+            'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+            'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+        StringBuffer pwd = new StringBuffer("");
+        Random r = new Random();
+        while (count < 15) {
+
+            i = Math.abs(r.nextInt(maxNum));
+
+            if (i >= 0 && i < str.length) {
+                pwd.append(str[i]);
+                count++;
+            }
+        }
+
+        return pwd.toString();
+    }
+}
+
+
+
+
+
+
